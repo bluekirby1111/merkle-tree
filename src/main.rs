80c493d818a9095fd88dbbb849d565e8ceba1908
Blue Kirby <blue.kirby@tutanota.com>
2022-12-13 23:30:35 +0000

    feat: add root and command arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{env, fs};
+
 use consts::{MAX_CHUNK_SIZE, MIN_CHUNK_SIZE};
 use error::Error;
 use hash::hash_all_sha256;
@@ -11,7 +13,16 @@ pub mod hash;
 pub mod types;
 
 fn main() {
-    println!("Merklize some data!");
+    let args: Vec<String> = env::args().collect();
+    let path = args[1].clone();
+    match fs::read(path.clone()) {
+        Ok(data) => {
+            let leaves: Vec<Node> = generate_leaves(data).unwrap();
+            let root = generate_data_root(leaves).unwrap();
+            println!("File merkle tree root: {:?}", root.hash);
+        }
+        Err(err) => println!("Error reading {} file: {}", path.clone(), err.to_string()),
+    }
 }
 
 pub fn generate_leaves(data: Vec<u8>) -> Result<Vec<Node>, Error> {
@@ -60,11 +71,19 @@ pub fn hash_branch(left: Node, right: Node) -> Result<Node, Error> {
     })
 }
 
+pub fn generate_data_root(mut nodes: Vec<Node>) -> Result<Node, Error> {
+    while nodes.len() > 1 {
+        nodes = build_layer(nodes).unwrap();
+    }
+    let root = nodes.pop().unwrap();
+    Ok(root)
+}
+
 #[cfg(test)]
 mod tests {
     use std::fs;
 
-    use crate::{build_layer, error::Error, generate_leaves, types::Node};
+    use crate::{build_layer, error::Error, generate_data_root, generate_leaves, types::Node};
 
     const ONE_MB_BINARY: &str = "res/1mb.bin";
 
@@ -98,6 +117,37 @@ mod tests {
             [
                 145, 252, 102, 63, 105, 86, 215, 195, 99, 148, 156, 153, 108, 133, 84, 33, 183,
                 193, 195, 63, 31, 184, 3, 202, 117, 86, 21, 85, 217, 148, 216, 103
+            ]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn should_generate_root() -> Result<(), Error> {
+        let data = fs::read(ONE_MB_BINARY).unwrap();
+        let leaves: Vec<Node> = generate_leaves(data).unwrap();
+        let root = generate_data_root(leaves).unwrap();
+        assert_eq!(
+            root.hash,
+            [
+                70, 162, 187, 117, 47, 193, 189, 49, 59, 4, 229, 155, 6, 73, 97, 9, 221, 249, 57,
+                16, 47, 250, 10, 117, 227, 14, 82, 240, 110, 214, 97, 116
+            ]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_valid_root_small_last_chunk() -> Result<(), Error> {
+        let data = vec![0; 256 * 1024 + 1];
+        // root id as calculate by arweave-js
+        let leaves: Vec<Node> = generate_leaves(data).unwrap();
+        let root = generate_data_root(leaves).unwrap();
+        assert_eq!(
+            root.hash,
+            [
+                145, 70, 225, 176, 161, 147, 24, 22, 11, 251, 142, 150, 71, 235, 137, 126, 212, 15,
+                12, 235, 176, 115, 164, 63, 203, 170, 102, 24, 219, 217, 174, 74
             ]
         );
         Ok(())
